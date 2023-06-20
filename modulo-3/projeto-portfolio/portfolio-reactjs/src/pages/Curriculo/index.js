a41ea24bd3173bdc79d79ae0fc06a5a105ebbae0
hvmazzola <98432153+hvmazzola@users.noreply.github.com>
2023-06-20 15:58:45 -0300

    fix: manejar pastas

--- modulo-3/projeto-portfolio/portfolio-reactjs/src/pages/Curriculo/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Curriculo = () => {
-  return (
-    <div>Curriculo</div>
-  )
-}
-
-export default Curriculo
--- modulo-3/projeto-portfolio/portfolio-reactjs/src/services/ferramentaService.js
+++
@@ -1,9 +0,0 @@
-import api from './api'
-
-const findAllFerramentas = () => {
-  return api.get('/ferramenta/findAll')
-  .then((response) => response)
-  .catch(err => err)
-}
-
-export default findAllFerramentas
